0737a4d46cb333a5ba80204c1db5dd8291e0a8b5
joycevnr <joycevitoria150@gmail.com>
2024-04-04 19:45:35 -0300

    problem completed

---
+++ python/1001 - Extremamente Básico/index.py
@@ -0,0 +1,17 @@
+"""import sys
+
+lines = sys.stdin.read().split('\n')
+
+a = int(lines.pop(0))
+b = int(lines.pop(0))
+
+print('X =', a + b)
+"""
+##outro
+
+a = int(input())
+b = int(input())
+
+x = a + b
+
+print('X =', x)
