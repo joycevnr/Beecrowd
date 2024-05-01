b15570a734706333fb5d2eab9fd1b332324ff88b
joycevnr <joycevitoria150@gmail.com>
2024-05-01 17:01:57 -0300

    problem completed

---
+++ python/1008 - Salário/index.py
@@ -0,0 +1,8 @@
+numero = int(input())
+horas_trabalhadas = int(input())
+valor_hora = float(input())
+
+salary = ( horas_trabalhadas * valor_hora )
+print(f'NUMBER = {numero}')
+
+print(f'SALARY = U$ {salary:.2f}')
