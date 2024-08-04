308f0b004ccc26c5d3088751d2c8a246c991386c
joycevnr <joycevitoria150@gmail.com>
2024-08-04 02:14:15 -0300

    problem

---
+++ python/p1/unidade_04/deduplica.py
@@ -0,0 +1,2 @@
+# Escreva um programa que identifica se uma dada sequência de valores inteiros está duplicada e imprime a lista deduplicada, ou seja, contendo apenas os valores não duplicados. Uma lista é dita duplicada se os valores da primeira metade forem exatamente iguais aos valores da segunda metade da lista em diante.
+
