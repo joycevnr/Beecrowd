3b9f3cc2be45216ef7df2c2c93237c9615c90901
joycevnr <joycevitoria150@gmail.com>
2024-08-04 01:45:48 -0300

    [int(x) for x in input().split()]

---
+++ python/other_exercises/for_sequencia.py
@@ -0,0 +1,4 @@
+lista = ["3", "4", "4"]
+## lista_de_inteiros = list(map(int, lista_de_strings)) # list() é usada para converter o resultado de map() de volta para uma lista.
+seq = [int(x) for x in input().split()]
+print(seq)
