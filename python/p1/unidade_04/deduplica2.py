7e1a49b5816aa497edf01a582f703af2916bd6f2
joycevnr <joycevitoria150@gmail.com>
2024-08-07 21:49:11 -0300

    unidade 4 - deduplica

---
+++ python/p1/unidade_04/deduplica2.py
@@ -0,0 +1,27 @@
+valores = input()
+lista = [int(x) for x in valores.split()]
+flag = True
+nova_lista = []
+
+if len(lista) % 2 != 0:
+    print(valores)
+else:
+    metade = len(lista) // 2
+    j = metade
+    for i in range(metade):
+        if lista[i] == lista[i + 1]:
+            nova_lista.append(lista[i])
+            flag = True
+        else:
+            flag = False 
+            break 
+    
+    if flag:
+        alt = nova_lista[0]
+        for i in range(1, len(nova_lista) - 1):
+             alt += ' ' + nova_lista[i]
+             print(alt)
+    else:
+        print(valores)
+            
+        
