6a414e46d9a7cb6b8f2415ea034c39bb9e3aba56
joycevnr <joycevitoria150@gmail.com>
2024-09-11 23:07:40 -0300

    resolved

---
+++ python/p1/unidade_06/unico.py
@@ -0,0 +1,32 @@
+
+def unico(string):
+    new_string = ""
+    for i in range(len(string)-1):
+        if string[i] != string[i+1]:
+            new_string += string[i]
+    if len(string) > 0:  # Verifica se a string não está vazia
+        new_string += string[-1]
+    return new_string
+            
+print(unico("aa***xxxzzb+"))
+    
+assert unico("aa***xxxzzb+++") == "a*xzb+"
+#assert unico("aa***xxxzzb+") == "a*xzb+"
+assert unico("") == ""
+
+# # Único
+
+# Escreva a função `unico(string)` que recebe uma string e 
+# retorna uma string formada  a partir da string original
+# pela substituição da sucessão de ocorrências (consecutivas) 
+# de um mesmo caractere por esse caractere. Não utilizar nenhuma lista
+# auxiliar. Por simplificação, assuma que a string não possui nem 
+# caracteres acentuados nem letras maiúsculas.
+
+# ## Exemplos de asserts
+
+# ```
+# assert unico("aa***xxxzzb+++") == "a*xzb+"
+# assert unico("") == ""
+# ```
+
