3491e356bb620caf268a981f357d180e9c5cc3f0
joycevnr <joycevitoria150@gmail.com>
2024-09-17 22:10:58 -0300

    completed

--- python/p1/unidade_07/afinidade_musical.py
+++ python/p1/unidade_07/afinidade_musical.py
@@ -1,3 +1,29 @@
+def tem_afinidade(l1, l2):
+    contador = 0
+    if len(l1) >= len(l2):
+        for i in range(len(l1)):
+            for j in range(len(l2)):
+                if l1[i] == l2[j]:
+                    contador += 1
+    elif len(l1) < len(l2):
+        for i in range(len(l1)):
+            for j in range(len(l2)):
+                if l1[i] == l2[j]:
+                    contador += 1
+    
+    if contador >= 3:
+        return True
+    else:
+        return False
+                    
+        
+
+
+l1 = ['zeze', 'bruno e marrone', 'joao', 'pedro', 'u2']
+l2 = ['zeze', 'joao', 'u2', 'jquest']
+assert tem_afinidade(l1, l2) == True
+
+
 # # Afinidade Musical
 
 # Uma métrica utilizada pelo facebook para sugerir
