a8ea1306648b977f2e7809ccb5ac8767e9adc159
joycevnr <joycevitoria150@gmail.com>
2024-09-24 22:55:46 -0300

    solution

---
+++ python/p1/unidade_07/desloca_direita.py
@@ -0,0 +1,35 @@
+  
+def desloca(lista, origem, destino):
+    # Salva o valor que será movido
+    valor = lista[origem]
+    
+    # Desloca os elementos para a esquerda
+    for i in range(origem, destino):
+        lista[i] = lista[i + 1]
+        print(lista)
+    
+    # Coloca o valor na posição de destino
+    lista[destino] = valor
+
+# Testes
+
+l1 = [2,6,9,11,13,5]
+desloca(l1, 2, 4)
+assert l1 == [2,6,11,13,9,5]
+
+# l1 = [0,1,2,3,4,5,6]
+# desloca(l1, 4, 6)
+# assert l1 == [0,1,2,3,5,6,4]
+
+# # Desloca o elemento para a direita
+
+# Implemente a função `desloca(lista, origem, destino)` que move o elemento na posição 
+# **origem** para a posição **destino** recebidas como argumento. Assuma que **origem** e 
+# **destino** são índices válidos e **origem** sempre é menor do que **destino**. Isto 
+# é, o deslocamento é para a direita.
+
+# Note também que não basta apenas trocar **origem** por **destino**, é preciso deslocar os 
+# elementos entre essas duas posições. Veja os exemplos de asserts para melhor entendimento 
+# da questão.
+
+# A função tem efeito colateral e não é permitido criar outra lista.
